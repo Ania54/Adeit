be0b2b394a67cae086d96500cae4cce74067d9d6
Ania <ania54345@gmail.com>
2024-12-14 21:53:44 +0100

    Revert the test change and format deploy.sh's output as a code block

--- Adeit.py
+++ Adeit.py
@@ -99,7 +99,7 @@ async def restart(ctx: discord.ApplicationContext):
 
 	# Run the deploy.sh script first
 	try:
-		await ctx.respond(subprocess.run(['bash', os.path.join(os.getcwd(), 'deploy.sh')], check=True, capture_output=True), ephemeral=True) # Run the script
+		await ctx.respond(f"```{subprocess.run(['bash', os.path.join(os.getcwd(), 'deploy.sh')], check=True, capture_output=True).stdout}```", ephemeral=True) # Run the script
 	except subprocess.CalledProcessError as e:
 		await ctx.respond(f"Nie udało się zaktualizować kodu: {e}")
 		return
