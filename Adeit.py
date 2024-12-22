c08bfe35cacab44a197694b8ada51f52cb4b720d
Ania <ania54345@gmail.com>
2024-12-22 12:34:08 +0100

    Print str(message) instead of message

--- Adeit.py
+++ Adeit.py
@@ -229,7 +229,7 @@ async def on_message(message: discord.Message):
 	if message.author.id == bot.user.id:
 		return
 
-	print(f"C: {message}")
+	print(f"C: {str(message)}")
 
 	new_content = original_content = message.content
 	
